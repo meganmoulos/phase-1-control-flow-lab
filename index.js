27bc7429013b77848ee990b32f975c1201731ee9
Megan <meganmoulos@gmail.com>
2022-08-07 10:54:50 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceFeet){
   // Write your code here!
+  if (distanceFeet < 400) {
+    return 'This one is on me!';
+  }
+  else if (distanceFeet >= 400 && distanceFeet < 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (distanceFeet >= 2000 && distanceFeet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distanceFeet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return (cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
